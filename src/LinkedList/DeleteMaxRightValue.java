43895763394463f4a117aa17841907e1c30382bf
alivia-23 <guin.al@husky.neu.edu>
2023-11-13 23:43:00 -0800

    delete right max value from linkedlist

---
+++ src/LinkedList/DeleteMaxRightValue.java
@@ -0,0 +1,22 @@
+package LinkedList;
+
+/**
+ * Given the head node of a singly linked list, modify the list such that any node that has a node with a greater value to its right gets removed.
+ * The function should return the head of the modified list.
+ *
+ * Examples:
+ *
+ * Input: 5 -> 3 -> 7 -> 4 -> 2 -> 1
+ * Output: 7 -> 4 -> 2 -> 1
+ * Explanation: 5 and 3 are removed as they have nodes with larger values to their right.
+ *
+ * Input: 1 -> 2 -> 3 -> 4 -> 5
+ * Output: 5
+ * Explanation: 1, 2, 3, and 4 are removed as they have nodes with larger values to their right.
+ */
+public class DeleteMaxRightValue {
+    public ListNode removeNode(ListNode head) {
+        ListNode cur = head;
+        return null;
+    }
+}
