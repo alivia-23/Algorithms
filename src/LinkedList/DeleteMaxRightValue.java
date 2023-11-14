998b43251970a7ee641a7b476b1804563b38cb01
alivia-23 <guin.al@husky.neu.edu>
2023-11-13 23:56:19 -0800

    delete greater value of right

--- src/LinkedList/DeleteMaxRightValue.java
+++ src/LinkedList/DeleteMaxRightValue.java
@@ -1,5 +1,7 @@
 package LinkedList;
 
+import java.util.Stack;
+
 /**
  * Given the head node of a singly linked list, modify the list such that any node that has a node with a greater value to its right gets removed.
  * The function should return the head of the modified list.
@@ -16,7 +18,19 @@ package LinkedList;
  */
 public class DeleteMaxRightValue {
     public ListNode removeNode(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
         ListNode cur = head;
-        return null;
+
+        while (cur != null) {
+            while (!stack.isEmpty() && stack.peek().val < cur.val) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                stack.peek().next = cur;
+            }
+            stack.push((cur));
+            cur = cur.next;
+        }
+        return stack.isEmpty() ? null : stack.get(0);
     }
 }
