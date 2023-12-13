04b7bc4b2996257474e14c68a95ce91aceb367b2
alivia-23 <guin.al@husky.neu.edu>
2023-12-12 23:47:12 -0800

    selection sort with worst case

---
+++ src/Sorting/SelectionSort.java
@@ -0,0 +1,22 @@
+package Sorting;
+
+public class SelectionSort {
+    public static void selectionSort(int[] vals) {
+        /* O(n^2) solution
+        for (int i = 0; i < vals.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < vals.length; j++) {
+                if (vals[j] < vals[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            swap(vals, minIndex, i);
+        }*/ 
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
