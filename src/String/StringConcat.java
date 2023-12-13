84d18adf707f92e78920ab48c3b0c771c158b4be
alivia-23 <guin.al@husky.neu.edu>
2023-12-12 23:47:40 -0800

    string concat

--- src/String/StringConcat.java
+++ src/String/StringConcat.java
@@ -1,2 +1,20 @@
-package String;public class StringConcat {
+package String;
+
+public class StringConcat {
+    public static int maxDifference (int[] vals) {
+        int max = 0;
+        for (int i=0; i < vals.length; i++) {
+            for (int j=0; j < vals.length; j++) {
+                if ((vals[i] - vals[j]) > max) {
+                    max = vals[i] - vals[j];
+                }
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] vals = {1,7,2,4,6,8};
+        int result = maxDifference(vals);
+        System.out.println(result);
+    }
 }
