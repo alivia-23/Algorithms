86e460472fe039521042f7a2f5cce4347aa34df9
alivia-23 <guin.al@husky.neu.edu>
2024-04-19 16:33:22 -0700

    remove elements of given val from array

---
+++ src/TwoPointerPatterns/RemoveElement.java
@@ -0,0 +1,24 @@
+package TwoPointerPatterns;
+
+/**
+ * Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+ * The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+ * Example 1:
+ * Input: nums = [3,2,2,3], val = 3
+ * Output: 2, nums = [2,2,_,_]
+ * Example 2:
+ * Input: nums = [0,1,2,2,3,0,4,2], val = 2
+ * Output: 5, nums = [0,1,4,0,3,_,_,_]
+ */
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
