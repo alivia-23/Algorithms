1abadb6e32b36ad722885613460d612e5e060fc7
alivia-23 <guin.al@husky.neu.edu>
2024-08-02 16:37:42 -0700

    group anagram

--- src/HashMap/GroupAnagram.java
+++ src/HashMap/GroupAnagram.java
@@ -1,2 +1,45 @@
-package HashMap;public class GroupAnagram {
+package HashMap;
+
+import java.util.*;
+
+/**
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+ * Example 1:
+ * Input: strs = ["eat","tea","tan","ate","nat","bat"]
+ * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ */
+public class GroupAnagram {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] chs = s.toCharArray();
+            Arrays.sort(chs);
+            String key = String.valueOf(chs);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+
+        for (String key : map.keySet()) {
+            List<String> bucket = map.get(key);
+            result.add(bucket);
+        }
+        return result;
+    }
+
+    public static void printAll(List<List<String>> result) {
+        for (List<String> s : result) {
+            System.out.print(s + " ");
+        }
+    }
+    public static void main(String[] args) {
+       String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        List<List<String>> result = groupAnagrams(strs);
+        printAll(result);
+    }
+
 }
