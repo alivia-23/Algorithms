74fe327d0124f6a043f9e179f00a936408471e95
alivia-23 <guin.al@husky.neu.edu>
2024-08-26 14:30:32 -0700

    stringValueOfAnd ToString

--- src/String/DifferenceBetweenStringValueOfAndToString.java
+++ src/String/DifferenceBetweenStringValueOfAndToString.java
@@ -1,2 +1,14 @@
-package String;public class DifferenceBetweenStringValueOfAndToString {
+package String;
+
+import java.util.Arrays;
+
+public class DifferenceBetweenStringValueOfAndToString {
+    public static void main(String[] args) {
+        char[] ch = new char[]{'c','a', 't'};
+//        System.out.println(String.valueOf(ch));
+//        System.out.println(ch.toString());   // it will give the object address
+        Arrays.sort(ch);
+        //Arrays.toString(ch); // gives the same result as String.valueOf(ch)
+        System.out.println(ch);
+    }
 }
