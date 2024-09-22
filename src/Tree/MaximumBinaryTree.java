0fb78f52c41dcb98c00cdd1686922991fdb430e4
alivia-23 <guin.al@husky.neu.edu>
2024-09-22 14:37:35 -0700

    maximum binary tree comment

--- src/Tree/MaximumBinaryTree.java
+++ src/Tree/MaximumBinaryTree.java
@@ -1,2 +1,33 @@
-package Tree;public class MaximumBinaryTree {
+package Tree;
+
+public class MaximumBinaryTree {
+    public TreeNode maxBinaryTree(int[] nums) {
+        if (nums == null) {
+            return null;
+        }
+        return build(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode build(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        // find the index in the array which contains the maximum value
+        int maxIdx = left;
+        for (int i = left; i <= right; i++) {
+            if (nums[i] > nums[maxIdx]) {
+                maxIdx = i;
+            }
+        }
+        //  create the root node with the max value
+        TreeNode root = new TreeNode(nums[maxIdx]);
+
+        // build the left and right subtree by recursively calling the build method
+        // left subtree will be formed by the left element of the max vaLue
+        // right subtree will be formed by the right element of the max value
+        root.left = build(nums, left, maxIdx - 1);
+        root.right = build(nums, maxIdx + 1, right);
+
+        return root;
+    }
 }
