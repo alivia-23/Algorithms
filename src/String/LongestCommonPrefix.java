64b3ed56de1db33b30e71307305d18687d8c7cb9
alivia-23 <guin.al@husky.neu.edu>
2024-09-27 00:43:49 -0700

    longest common prefix

--- src/String/LongestCommonPrefix.java
+++ src/String/LongestCommonPrefix.java
@@ -1,5 +1,7 @@
 package String;
 
+import java.util.Arrays;
+
 /**
  * Write a function to find the longest common prefix string amongst an array of strings.
  * If there is no common prefix, return an empty string "".
@@ -12,6 +14,10 @@ package String;
  * Output: ""
  */
 public class LongestCommonPrefix {
+
+    // Brute Force approach ,
+    // Time Complexity - O(M.N) where M stands for the length of the base string and N stands for the length of the string array.
+    // Space Complexity - O (1)
     public static String longestPrefix(String[] str) {
         String baseString = str[0];
         StringBuilder commonPrefix = new StringBuilder();
@@ -28,8 +34,28 @@ public class LongestCommonPrefix {
         return commonPrefix.toString();
     }
 
+    // Optimized approach by sorting
+    // Time complexity - O(m.k.logn)
+    // Space Complexity - O(1)
+
+    // "clap", "claw", "clan", "clean" --Sort--> "clan", "clap", "clap", "clean"
+    public static String longestCommonPrefix(String[] str) {
+        Arrays.sort(str);
+        char[] first = str[0].toCharArray();
+        char[] last = str[str.length - 1].toCharArray();
+        StringBuilder commonPrefix = new StringBuilder();
+
+        for (int i = 0; i < first.length; i++) {
+            if (first[i] != last[i]) {
+                return commonPrefix.toString();
+            }
+            commonPrefix.append(first[i]);
+        }
+        return commonPrefix.toString();
+    }
     public static void main(String[] args) {
         String[] str = {"flower","flow","flight"};
-        System.out.println(longestPrefix(str));
+        System.out.println(longestPrefix(str)); // Brute Force approach
+        System.out.println(longestCommonPrefix(str)); // optimized approach
     }
 }
